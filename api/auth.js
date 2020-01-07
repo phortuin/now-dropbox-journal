4a3e7e650090873f51562674d062262a3dabbee7
Anne Fortuin <phortuin@gmail.com>
2020-01-07 15:09:16 +0100

    chore: whitespace

--- api/auth.js
+++ api/auth.js
@@ -5,31 +5,31 @@ const redirectUrl = require('../lib/redirect-url')
 const { errors, cookies, redirects, queryParams } = require('../lib/constants')
 
 module.exports = async (request, response) => {
-    const code = request.query[queryParams.CODE]
-    const state = request.query[queryParams.STATE]
-    const stateFromCookie = request.cookies[cookies.STATE]
+	const code = request.query[queryParams.CODE]
+	const state = request.query[queryParams.STATE]
+	const stateFromCookie = request.cookies[cookies.STATE]
 
-    if (!code || !state) {
-        response.writeHead(403)
-        response.end(errorPage(errors.NO_CODE))
-    }
+	if (!code || !state) {
+		response.writeHead(403)
+		response.end(errorPage(errors.NO_CODE))
+	}
 
-    if (state !== stateFromCookie) {
-        response.writeHead(403)
-        response.end(errorPage(errors.INVALID_STATE))
-    }
+	if (state !== stateFromCookie) {
+		response.writeHead(403)
+		response.end(errorPage(errors.INVALID_STATE))
+	}
 
-    try {
-	    const token = await dropbox.getAccessTokenFromCode(redirectUrl(request), code)
-	    response.writeHead(302, {
-	        Location: redirects.DEFAULT,
-	        'Set-Cookie': cookie.set(cookies.TOKEN, token)
-	    })
-	    response.end()
-    } catch (error) {
-    	response.writeHead(error.status || 500, {
-    		'Set-Cookie': cookie.clear(cookies.TOKEN)
-    	})
-    	response.end(errorPage(error.error && error.error.error_description || errors.UNKNOWN))
-    }
+	try {
+		const token = await dropbox.getAccessTokenFromCode(redirectUrl(request), code)
+		response.writeHead(302, {
+			Location: redirects.DEFAULT,
+			'Set-Cookie': cookie.set(cookies.TOKEN, token)
+		})
+		response.end()
+	} catch (error) {
+		response.writeHead(error.status || 500, {
+			'Set-Cookie': cookie.clear(cookies.TOKEN)
+		})
+		response.end(errorPage(error.error && error.error.error_description || errors.UNKNOWN))
+	}
 }
--- api/index.js
+++ api/index.js
@@ -6,27 +6,27 @@ const randomString = require('../lib/random-string')
 const { cookies, redirects } = require('../lib/constants')
 
 module.exports = (request, response) => {
-    const token = request.cookies[cookies.TOKEN]
-    if (token) {
-        response.writeHead(302, {
-        	Location: redirects.DEFAULT,
-            'Set-Cookie': cookie.clear(cookies.STATE)
-        })
-        response.end()
-    } else {
-    	try {
-	        const state = randomString()
-	        const authUrl = dropbox.getAuthenticationUrl(redirectUrl(request), state, 'code')
-	        response.writeHead(302, {
-	            Location: authUrl,
-	            'Set-Cookie': cookie.set(cookies.STATE, state)
-	        })
-	        response.end()
+	const token = request.cookies[cookies.TOKEN]
+	if (token) {
+		response.writeHead(302, {
+			Location: redirects.DEFAULT,
+			'Set-Cookie': cookie.clear(cookies.STATE)
+		})
+		response.end()
+	} else {
+		try {
+			const state = randomString()
+			const authUrl = dropbox.getAuthenticationUrl(redirectUrl(request), state, 'code')
+			response.writeHead(302, {
+				Location: authUrl,
+				'Set-Cookie': cookie.set(cookies.STATE, state)
+			})
+			response.end()
 		} catch (error) {
 			response.writeHead(error.status || 500, {
 				'Set-Cookie': cookie.clear(cookies.TOKEN)
 			})
 			response.end(errorPage(error.error && error.error.error_description || errors.UNKNOWN))
 		}
-    }
+	}
 }
